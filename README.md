4236fe22ac114ba65a585c61a890844bf4bbc8a4
jayeshkv <jvijayaraghavalu@umassd.edu>
2017-01-14 14:51:54 -0500

    added static url | autor & rating prints

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-amazon-review-scraper
 An Amazon review parser written in GO
+
+This is my first time experimenting with Go
+There will be mistakes, i will do my best to keep it optimized.
--- parser.go
+++ parser.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+"github.com/PuerkitoBio/goquery"
 "bytes" //for appending url
 "fmt"
+"log"
 )
 var(
 prdUrl bytes.Buffer //holds the temp url
 prdId string //gets product id from user
 )
 func main() {
-	fmt.Print("Enter the product ID: ")
+/*	fmt.Print("Enter the product ID: ")
 	fmt.Scanln(&prdId)
 	prdUrl.WriteString(prdId)
-	fmt.Println(prdUrl.String())
-	
+*/	doc, err := goquery.NewDocument(prdUrl.String())
+	if err != nil {
+    	log.Fatal(err)
+	}else {
+
+
+	doc.Find(".a-section .review").Each(func(i int, s *goquery.Selection){
+	author:=s.Find(".author").Text()
+	rating:=s.Find(".a-icon-alt").Text()
+	fmt.Print("Review by ",author)
+	fmt.Println("| Rating ",rating)
+	})
+	}
 }
+
 func init() {
-prdUrl.WriteString("http://amazon.com/review/")
+prdUrl.WriteString("http://amazon.com/review/1484212371")
 }
