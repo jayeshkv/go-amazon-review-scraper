361c1868c185a00b158ad80dac853466eeb928d9
jayeshkv <jvijayaraghavalu@umassd.edu>
2017-01-14 14:58:20 -0500

    prints out author, rating & review based on product id

--- parser.go
+++ parser.go
@@ -11,10 +11,10 @@ prdUrl bytes.Buffer //holds the temp url
 prdId string //gets product id from user
 )
 func main() {
-/*	fmt.Print("Enter the product ID: ")
+	fmt.Print("Enter the product ID: ")
 	fmt.Scanln(&prdId)
 	prdUrl.WriteString(prdId)
-*/	doc, err := goquery.NewDocument(prdUrl.String())
+	doc, err := goquery.NewDocument(prdUrl.String())
 	if err != nil {
     	log.Fatal(err)
 	}else {
@@ -23,12 +23,12 @@ func main() {
 	doc.Find(".a-section .review").Each(func(i int, s *goquery.Selection){
 	author:=s.Find(".author").Text()
 	rating:=s.Find(".a-icon-alt").Text()
-	fmt.Print("Review by ",author)
-	fmt.Println("| Rating ",rating)
+	review:=s.Find(".review-text").Text()
+	fmt.Printf("Review# %d \n Author: %s \n Rating %s \n Review: %s \n",i, author, rating, review)
 	})
 	}
 }
 
 func init() {
-prdUrl.WriteString("http://amazon.com/review/1484212371")
+prdUrl.WriteString("http://amazon.com/review/")
 }
